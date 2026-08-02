40d9577f847ff7ac1ad587b48f1aba8a5b7b382a
司金龙 <sobeautytech@gmail.com>
2026-08-02 14:15:58 +0200

    feat(report): add shared print button

--- src/components/report/index.ts
+++ src/components/report/index.ts
@@ -2,6 +2,7 @@ export * from "./report-content-block";
 export * from "./report-header";
 export * from "./report-metadata";
 export * from "./report-navigation";
+export * from "./report-print-button";
 export * from "./report-section";
 export * from "./report-shell";
 export * from "./report-state";
