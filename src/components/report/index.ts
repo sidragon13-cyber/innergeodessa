7c778c0698713ef1634edc4511cc63386fbdca21
司金龙 <sobeautytech@gmail.com>
2026-08-02 14:12:11 +0200

    feat(report): add shared report state

--- src/components/report/index.ts
+++ src/components/report/index.ts
@@ -1,3 +1,5 @@
 export * from "./report-content-block";
+export * from "./report-navigation";
 export * from "./report-section";
+export * from "./report-state";
 export * from "./report-table-of-contents";
